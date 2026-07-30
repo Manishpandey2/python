f26d5395211fbd421bd81bf2f1a6a7738917e96c
Manishpande2 <biraalogang@gmail.com>
2026-07-30 09:36:02 +0545

    first commit

--- first.py
+++ first.py
@@ -1,6 +1,11 @@
-with open("file.txt","r") as file:
-    conent = file.read()
-print(conent)
+# with open("file.txt","r") as file:
+#     conent = file.read()
+# print(conent)
 
-with open("todo.txt","w") as file:
-    file.write("Learn Python Build AI Agent Practice Daily")
+# with open("todo.txt","w") as file:
+#     file.write("Learn Python Build AI Agent Practice Daily")
+
+with open("students.txt","r") as file:
+    content = file.read()
+
+print(content)

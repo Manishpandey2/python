1bbdb09625c28023326763aa553d1da57b06ef6c
Manishpande2 <biraalogang@gmail.com>
2026-07-31 08:22:08 +0545

    second version using random module

--- quotes.py
+++ quotes.py
@@ -1,3 +1,30 @@
+# try:
+#     quotes = [
+#     {
+#         "text": "Success is not final.",
+#         "author": "Winston Churchill"
+#     },
+#     {
+#         "text": "Stay hungry, stay foolish.",
+#         "author": "Steve Jobs"
+#     },
+#     {
+#         "text": "Code is like humor.",
+#         "author": "Cory House"
+#     }
+#     ]
+#     choice = int(input("Choose a quote(0-2): "))
+
+#     print("----------------------------")
+
+#     print(quotes[choice]["text"])
+#     print(f"- {quotes[choice]['author']}")
+#     print("----------------------------")
+# except IndexError:
+#     print("Invalid Choice")
+# except ValueError:
+#     print("Please enter a number")
+import random
 try:
     quotes = [
     {
@@ -13,12 +40,11 @@ try:
         "author": "Cory House"
     }
     ]
-    choice = int(input("Choose a quote(0-2): "))
-
+    quote = random.choice(quotes)
     print("----------------------------")
 
-    print(quotes[choice]["text"])
-    print(f"- {quotes[choice]['author']}")
+    print(quote["text"])
+    print(f"- {quote['author']}")
     print("----------------------------")
 except IndexError:
     print("Invalid Choice")

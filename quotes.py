da2925176bcafd54882673ecfd6e5d528ad1c9c8
Manishpande2 <biraalogang@gmail.com>
2026-07-31 15:24:29 +0545

    version 4

--- quotes.py
+++ quotes.py
@@ -53,13 +53,55 @@
 
 
 # Third Version
+# import random
+
+# try:
+    # quotes = [
+    #     {
+
+    #     },
+    #     {
+    #         "text" : "nepal hami nepali sabaiko sajha ghar ho",
+    #         "author": "Manish Pandey"
+    #     },
+    #     {
+    #         "text" : "Happy birthday to you",
+    #         "author": "ramesh kaka"
+    #     },
+    #     {
+    #         "text" : "oh ho vailai kasto chitikka dekhiyo",
+    #         "author": "kapada pasale"
+    #     },
+    #     {
+    #         "text" : "good morning, tell me how can i help you",
+    #         "author": "chatgpt"
+    #     },
+    # ]
+    
+#     quote = random.choice(quotes)
+#     print("==== Random Qoute Generator ====")
+
+#     print("1. Show Qoute")
+#     print("2. Exit")
+
+#     choose = int(input("Choose: "))
+
+
+
+#     if choose == 1:
+#         print(quote["text"])
+#         print(f"- {quote['author']}")
+#     else:
+#         print("Good Bye!")
+# except IndexError:
+#     print("Invalid Choice!")
+# except ValueError:
+#     pirnt("Enter number 1-2 only")
+
+#another version
+
 import random
-
-try:
-    quotes = [
-        {
-
-        },
+quotes= [
         {
             "text" : "nepal hami nepali sabaiko sajha ghar ho",
             "author": "Manish Pandey"
@@ -77,23 +119,32 @@ try:
             "author": "chatgpt"
         },
     ]
-    
-    quote = random.choice(quotes)
-    print("==== Random Qoute Generator ====")
 
+print("=== Random Qoute Generator ====")
+
+
+    
+while True:
+     
     print("1. Show Qoute")
     print("2. Exit")
 
-    choose = int(input("Choose: "))
-
-
-
+    quote = random.choice(quotes)
+    try:
+        choose = int(input("Choose: "))
+    except ValueError: 
+        print("Enter a number")
+        continue
     if choose == 1:
+        print("-----------------------")
+
         print(quote["text"])
-        print(f"- {quote['author']}")
-    else:
+        print(f" - {quote['author']}")
+    
+        print("-----------------------")
+    
+    elif choose ==2:
         print("Good Bye!")
-except IndexError:
-    print("Invalid Choice!")
-except ValueError:
-    pirnt("Enter number 1-2 only")
+        break
+    else:
+        print("Invalid Choice")

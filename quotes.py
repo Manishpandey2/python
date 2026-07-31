65f12a2ee27709db875237d97772167f3f1d2891
Manishpande2 <biraalogang@gmail.com>
2026-07-31 09:11:27 +0545

    third version

--- quotes.py
+++ quotes.py
@@ -17,36 +17,83 @@
 
 #     print("----------------------------")
 
-#     print(quotes[choice]["text"])
-#     print(f"- {quotes[choice]['author']}")
+# #     print(quotes[choice]["text"])
+# #     print(f"- {quotes[choice]['author']}")
+# #     print("----------------------------")
+# # except IndexError:
+# #     print("Invalid Choice")
+# # except ValueError:
+# #     print("Please enter a number")
+# import random
+# try:
+#     quotes = [
+#     {
+#         "text": "Success is not final.",
+#         "author": "Winston Churchill"
+#     },
+#     {
+#         "text": "Stay hungry, stay foolish.",
+#         "author": "Steve Jobs"
+#     },
+#     {
+#         "text": "Code is like humor.",
+#         "author": "Cory House"
+#     }
+#     ]
+#     quote = random.choice(quotes)
+#     print("----------------------------")
+
+#     print(quote["text"])
+#     print(f"- {quote['author']}")
 #     print("----------------------------")
 # except IndexError:
 #     print("Invalid Choice")
 # except ValueError:
 #     print("Please enter a number")
+
+
+# Third Version
 import random
+
 try:
     quotes = [
-    {
-        "text": "Success is not final.",
-        "author": "Winston Churchill"
-    },
-    {
-        "text": "Stay hungry, stay foolish.",
-        "author": "Steve Jobs"
-    },
-    {
-        "text": "Code is like humor.",
-        "author": "Cory House"
-    }
-    ]
-    quote = random.choice(quotes)
-    print("----------------------------")
+        {
 
-    print(quote["text"])
-    print(f"- {quote['author']}")
-    print("----------------------------")
+        },
+        {
+            "text" : "nepal hami nepali sabaiko sajha ghar ho",
+            "author": "Manish Pandey"
+        },
+        {
+            "text" : "Happy birthday to you",
+            "author": "ramesh kaka"
+        },
+        {
+            "text" : "oh ho vailai kasto chitikka dekhiyo",
+            "author": "kapada pasale"
+        },
+        {
+            "text" : "good morning, tell me how can i help you",
+            "author": "chatgpt"
+        },
+    ]
+    
+    quote = random.choice(quotes)
+    print("==== Random Qoute Generator ====")
+
+    print("1. Show Qoute")
+    print("2. Exit")
+
+    choose = int(input("Choose: "))
+
+
+
+    if choose == 1:
+        print(quote["text"])
+        print(f"- {quote['author']}")
+    else:
+        print("Good Bye!")
 except IndexError:
-    print("Invalid Choice")
+    print("Invalid Choice!")
 except ValueError:
-    print("Please enter a number")
+    pirnt("Enter number 1-2 only")

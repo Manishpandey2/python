c6be31653a06384d80ad40e80efeee91d9eb4fa2
Manishpande2 <biraalogang@gmail.com>
2026-08-03 15:28:08 +0545

    first ai api call

--- ai_chatbot.py
+++ ai_chatbot.py
@@ -1,5 +1,14 @@
+from google import genai
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-print(api_key)
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model="models/gemini-3.6-flash",
+    contents="what can you do?"
+)
+
+print(response.text)

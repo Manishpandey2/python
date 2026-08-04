d3cef95264fc6ad231ab2b7e2e9541c057e6b439
Manishpande2 <biraalogang@gmail.com>
2026-08-04 07:59:09 +0545

    version 2 completed now chatbot can remember chat history

--- ai_chatbot.py
+++ ai_chatbot.py
@@ -1,14 +1,42 @@
-from google import genai
 import os
+from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+api_key = os.getenv("GEMINI_API_KEY")
 
-response = client.models.generate_content(
-    model="models/gemini-3.6-flash",
-    contents="what can you do?"
+client = genai.Client(api_key=api_key)
+# model = "models/gemini-3.6-flash"
+chat = client.chats.create(
+    model = "models/gemini-3.6-flash"
 )
+# conversation = []
+while True:
+    content = input("You: ")
+    if content.strip() == "exit":
+        print("Goodbye!")
+        break
+    if content.strip() == "":
+        print("Please ask something.")
+        continue
+    # conversation.append(
+    #     {
+    #         "role": "user",
+    #         "content": content
+    #     }
+    # )
+    # response = client.models.generate_content(
+    #     model = model,
+    #     contents = conversation
+    # )
+    response = chat.send_message(content)
 
-print(response.text)
+    print(f"AI: {response.text}")
+
+    # conversation.append(
+    #     {
+    #         "role": "model",
+    #         "content": response.text
+    #     }
+    # )
